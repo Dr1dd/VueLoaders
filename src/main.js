9118d504e509d44f223fdcf2b02ebad0bacf08e1
Laurynas Leišis <lauris.leisis@gmail.com>
2020-08-10 12:41:54 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import component from './Loader.vue'
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+function install(Vue) {
+    if (install.installed) return;
+    install.installed = true;
+    Vue.component("Loader", component);
+  }
+  
+  const plugin = {
+    install
+  };
+  
+  let GlobalVue = null;
+  if (typeof window !== "undefined") {
+    GlobalVue = window.Vue;
+  } else if (typeof global !== "undefined") {
+    GlobalVue = global.vue;
+  }
+  
+  if (GlobalVue) {
+    GlobalVue.use(plugin);
+  }
+  
+  component.install = install;
+  
+  export default component;
